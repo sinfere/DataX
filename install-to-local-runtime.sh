37cb26e79c75571344349761e4b45e112daa4ee1
dd <sinfere@gmail.com>
2024-04-28 09:29:43 +0800

    feat: update build path

--- install-to-local-runtime.sh
+++ install-to-local-runtime.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 set -o pipefail
 
 DIR=`realpath $(dirname $0)`
 
-rsync -avz -t $DIR/target/datax/datax/ $DCORE/pro/datax
+rsync -avz -t $DIR/target/datax/datax/ $DSTORE/git/BASE/datax/datax-package
